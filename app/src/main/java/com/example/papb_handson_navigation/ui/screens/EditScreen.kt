a2273fc5e61acb19d7a97e426789f578e883d277
VlynXx <rendiadipradananh@gmail.com>
2025-10-15 12:03:45 +0700

    feat: Add EditScreen for item editing

---
+++ app/src/main/java/com/example/papb_handson_navigation/ui/screens/EditScreen.kt
@@ -0,0 +1,36 @@
+package com.example.papb_handson_navigation.ui.screens
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+
+@Composable
+fun EditScreen(navController: NavController, id: String?) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(20.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "Edit Item",
+            style = MaterialTheme.typography.headlineSmall
+        )
+        Spacer(modifier = Modifier.height(12.dp))
+        Text(
+            text = "ID Item: $id",
+            style = MaterialTheme.typography.bodyLarge
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+        Button(onClick = { navController.popBackStack() }) {
+            Text(text = "Kembali")
+        }
+    }
+}
